8701af14fdb2fb9a9dc28d8268f651fb4f6d80f9
Manish Aggarwal <manishaggarwal8304@gmail.com>
2024-07-28 01:52:58 +0530

    somechangesincss3

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -24,7 +24,7 @@ export default function Sidebar(props) {
     return (
         <section className="pane sidebar">
             <div className="sidebar--header">
-                <img src="https://tse1.mm.bing.net/th?id=OIP.Kj4CHhh5IEbk4t9eKkRjZgAAAA&pid=Api&rs=1&c=1&qlt=95&w=108&h=108"/>
+                <img className="sidebar-logo"src="https://tse1.mm.bing.net/th?id=OIP.Kj4CHhh5IEbk4t9eKkRjZgAAAA&pid=Api&rs=1&c=1&qlt=95&w=108&h=108"/>
                 <h3>My Notes</h3>
                 <button className="new-note" onClick={props.newNote}>+</button>
             </div>
